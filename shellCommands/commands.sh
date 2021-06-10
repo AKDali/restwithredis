7052779299d62f0e7286d80bed50d92f38540c9e
AKDali <abdelkader.dali@icloud.com>
2021-06-10 14:59:24 +0200

    shell commands 2

--- shellCommands/commands.sh
+++ shellCommands/commands.sh
@@ -8,13 +8,15 @@ docker login
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml push node-app
 
 # on Prod ENV
-## install docker 
+## install docker (ubuntu)
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 docker --version
+## install docker (amazon linux 2) go and check below repo
+https://gist.github.com/npearce/6f3c7826c7499587f00957fee62f8ee9
 
 ## install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -v
 
